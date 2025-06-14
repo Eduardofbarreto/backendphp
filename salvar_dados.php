d050228f2f13da885f6c7a13506555ef8a59df97
Eduardofbarreto <fbarreto.eduardo>
2025-06-14 11:01:03 -0300

    melhorando

--- salvar_dados.php
+++ salvar_dados.php
@@ -15,6 +15,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     
     $nome = $_POST['nome'] ?? '';
     $idade = $_POST['idade'] ?? '';
+    $endereco = $POST['endereco'] ?? '';
 
     
     if (empty($nome)) {
