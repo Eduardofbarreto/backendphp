ce04ca1cb7e3a0e38fa6bf3cb6d94ffffc212ff0
Eduardofbarreto <fbarreto.eduardo>
2025-06-19 12:20:20 -0300

    seguindo por aqui

--- login/salvar_pessoa.php
+++ login/salvar_pessoa.php
@@ -1,27 +1,23 @@
 <?php
+require_once 'conexao.php';
 
-if($_SERVER["RESQUEST_METHOD"] == "POST"){
-    $nome = isset($_POST['nome']) ? htmlspecialchars($_POST['nome']) : 'Não informado';
-    $email = isset($_POST['email']) ? htmlspecialchars($_POST['email']) : 'Não informado';
-    $dataNascimento = isset($_POST['dataNascimento']) ? htmlspecialchars($_POST['dataNascimento']) : 'Não informado';
-    $cpf = isset($_POST['cpf']) ? htmlspecialchars($_POST['cpf']) : 'Não informado';
-    $telefone = isset($_POST['telefone']) ? htmlspecialchars($_POST['telefone']) : 'Não informado';
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $nome = htmlspecialchars(trim($_POST['nome']));
+    $email = htmlspecialchars(trim($_POST['email']));
+    $data_nascimento_str = htmlspecialchars(trim($_POST['data_nascimento']));
+    $cpf = htmlspecialchars(trim($_POST['cpf']));
+    $telefone = htmlspecialchars(trim($_POST['telefone']));
 
-    echo "<h1>Dados recebidos com sucesso!</h1>";
-    echo "<p>Olá, <strong>$nome</strong>! Sey cadastro foi rececido.</p>";
-
-    echo "<div class='data-item'><strong>Nome: </strong> $nome </div>";
-    echo "<div class'data-item'><strong>E-mail: </strong> $email </div>";
-    echo "<div class='data-item'><strong>Data de Nascimento: </strong> $dataNascimento </div>";
-    echo "<div class='data-item'><strong>CPF:</strong> $cpf </div>";
-    echo "<div class='data-item'><strong>Telefone: </strong> $telefone </div>";
-
-    echo "<p><em>Estes dados são apenas exibidos!</em></p>";
-    echo '<a href="index.html" class="back-button">Voltar ao formulário</a>';
-} else {
-    echo "<h1>Acesso inválido!</h1>";
-    echo "<p>Este arquivo deve ser acessado via envio de formulário.</p>";
-    echo '<a href="index.html" class="back-button">Voltar ao formulário.</a>';
+    $data_nascimento = null;
+    if(!empty($data_nascimento_str)){
+        $data_obj = DateTime::createFromFormat('d-m-Y', $data_nascimento_str);
+        if($data_obj){
+            $data_nascimento = $data_obj->format('Y-m-d');
+        }else{
+            echo "Erro: Formato de data inválido. Use DD-MM-AAAA.<br>";
+        }
+    }
 }
+
 
 ?>
