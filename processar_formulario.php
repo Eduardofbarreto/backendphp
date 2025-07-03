b29f110122d8a62b87be686666a89fb866772255
Eduardofbarreto <fbarreto.eduardo>
2025-07-03 20:16:57 -0300

    fazendo o php

--- processar_formulario.php
+++ processar_formulario.php
@@ -1,10 +1,21 @@
 <?php
 
+$host = "edu_barreto";
+$port = "5432";
+$dbname = "meu_novo_banco_de_dados";
+$user = "postgres";
+$password = "root";
+
+$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+
+if(!$conn){
+    die("Connection failed: " . preg_last_error());
+}
+
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-
-    $nome = isset($_POST['nome']) ? htmlspecialchars($_POST['nome']) : '';
-    
-
+    $nome = pg_escape_string($conn, $_POST['nome']);
+    $endereco = pg_escape_string($conn, $_POST['endereco']);
+    $data_nascimento = pg_escape_string($conn, $_POST['dataNascimento']);
 }
 
 ?>
