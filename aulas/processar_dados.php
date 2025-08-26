2a527dad669a84337a1ed8a899b9a4f94f518524
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-08-25 21:51:40 -0300

    if elseif else

--- aulas/processar_dados.php
+++ aulas/processar_dados.php
@@ -6,10 +6,18 @@
 
             $idade = intval($_POST['idade']);
 
-            if($idade >=18){
-                echo "Você tem " . $idade . " anos e é maior de idade";
+            if($idade < 0){
+                echo "Idade inválida!!";
+            }elseif($idade <=8){
+                echo "Você tem " . $idade . " anos e é uma CRIANÇA!";
+            }elseif($idade <= 12){
+                echo "Você tem " . $idade . " anos e é um pré-adolescente!";
+            }elseif($idade <=18){
+                echo "Você tem " . $idade . " anos e é um adolescente!";
+            }elseif($idade <=60){
+                echo "Você tem " . $idade . " anos e é um adulto!";
             }else{
-                echo "Você tem " . $idade . " anos e é menor de idade";
+                echo "Você tem " . $idade . " anos e é um sênior!";
             }
         }else{
             echo "Dados inválidos!";
