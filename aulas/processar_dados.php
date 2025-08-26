76fba747bb446b4bd9ea75fcff069932bf48e0a1
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-08-26 18:27:59 -0300

    Brincando com as cores

--- aulas/processar_dados.php
+++ aulas/processar_dados.php
@@ -1,24 +1,23 @@
 <?php
 
-    if($_SERVER['REQUEST_METHOD'] == "POST"){
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        if(isset($_POST['cor'])){
 
-        if(isset($_POST['numero'])){
+            $cor = $_POST['cor'];
 
-            $numero = intval($_POST['numero']);
-
-            $tabuada = array(1,2,3,4,5,6,7,8,9,10);
-
-            for($i = 0; $i <10; $i++){
-                $resultado = $numero * $tabuada[$i] . "<br>";
-                echo $numero . " x " . $tabuada[$i] . " = " . $resultado . "<br>";
+            switch($cor){
+                case "azul":
+                echo "Você escolheu a cor " . $cor;
+            break;
+                case "vermelho":
+                echo "Você escolheu a cor " . $cor;
+            break;
+                default:
+                echo "Nenhuma cor válida!";
             }
-
-
         }else{
-            echo "Dados inválidos!";
+            echo "Informações incorretas!";
         }
-
-}
-    
+    }
 
 ?>
