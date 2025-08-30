f419a6bb4a99d265b497cdaf7ec77c1a8d6daf14
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-08-30 11:37:36 -0300

    adivinhação com número constante

--- aulas/processar_dados.php
+++ aulas/processar_dados.php
@@ -1,36 +1,32 @@
 <?php
 
-
 session_start();
 
 if(!isset($_SESSION['numero_secreto'])){
-    $_SESSION['numero_secreto'] = rand(1, 100);
+    $_SESSION['numero_secreto'] = 10;
 }
 
 if(isset($_POST['palpite'])){
     $palpite = $_POST['palpite'];
 
-    $numeroSecreto = $_SESSION['numero_secreto'];
+    $numero_secreto = $_SESSION['numero_secreto'];
 
-    echo "<h1>Resultado do seu Palpite</h1>";
+    echo "<h1>Resultado do seu palpite!</h1>";
 
-    if($palpite < $numeroSecreto){
+    if($palpite < $numero_secreto){
         echo "<p>Seu palpite é muito baixo! Tente um número maior!</p>";
-    }elseif($palpite > $numeroSecreto){
+    }elseif($palpite > $numero_secreto){
         echo "<p>Seu palpite é muito alto! Tente um número menor!</p>";
     }else{
-        echo "<h2>Parabéns! Você acertou o número $numeroSecreto!</h2>";
-        echo "<p>Vamos começar um novo jogo.</p>";
+        echo "<h2>Parabéns!! Você acertou o número $numero_secreto!</h2>";
+        echo "<p>Vamos começar um novo jogo!</p>";
 
         session_destroy();
     }
 
-    echo "<a href='index.html'>Tentar novamente</a>";
+    echo "<a href='index.html'>Tentar novamente!</a>";
 }else{
     echo "<h1>Bem-vindo ao jogo de adivinhação!</h1>";
     echo "<p>Digite seu palpite e clique em adivinhar!</p>";
-    echo "<a href='index.html'>Voltar ao jogo</a>";
+    echo "<a href='index.html'>Voltar ao jogo!</a>";
 }
-
-
-?>
