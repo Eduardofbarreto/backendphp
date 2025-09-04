f5dc23e370a413055e7b65b17faf63cdd8fc450b
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-09-04 16:52:57 -0300

    calculadora em php

--- aulas/processar_dados.php
+++ aulas/processar_dados.php
@@ -1,22 +1,38 @@
 <?php
 
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
-
-    if(isset($_POST['num1']) && isset($_POST['num2']) && isset($_POST['num3'])){
-
+    if(isset($_POST['num1']) && isset($_POST['num2'])){
         $num1 = $_POST['num1'];
         $num2 = $_POST['num2'];
-        $num3 = $_POST['num3'];
 
-        if($num1 > $num2 && $num1 > $num3){
-            echo $num1 . " é o maior número de todos!";
-        }elseif($num2 > $num1 && $num2 > $num3){
-            echo $num2 . " é o maior número de todos!";
-        }else{
-            echo $num3 . " é o maior número de todos!";
+        $op = $_POST['op'];
+
+        switch($op){
+            case '+':
+                $soma = $num1 + $num2;
+                echo "O resultado da soma é " . $soma . ".";
+            break;
+            case '-' :
+                $subt = $num1 - $num2;
+                echo "O resultado da subtração é " . $subt . ".";
+            break;
+            case '*':
+                $mult = $num1 * $num2;
+                echo "O resultado da multiplicação é " . $mult . ".";
+            break;
+            case '/':
+                if($num2 == 0){
+                    echo "Erro ao tentar dividir por 0!";
+                }else{
+                    $div = $num1 / $num2;
+                echo "O resultado da divisão é " . $div . ".";
+                }
+            break;
+            default:
+                echo "Algo está errado!";
         }
     }else{
-        echo "Dados inválidos!";
+        echo "Preencha os campos!";
     }
 }
 
