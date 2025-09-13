7ecef456d2bbf70d8fcee61ce6d9d25189d1c409
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-09-13 10:45:03 -0300

    get para passwords

--- basic/index.php
+++ basic/index.php
@@ -8,22 +8,36 @@
 </head>
 <body>
 
+<div class="form">
+    <form action="index.php" method="GET">
+        <label for="user">User:</label>
+        <input type="text" id="user" name="user">
+        <br><br>
+        <label for="senha">Senha:</label>
+        <input type="password" id="senha" name="senha">
+        <br><br>
+        <button type="submit">Cadastrar</button>
+    </form>
+</div>
+
 <?php
 
-$nome = "Eduardo F. Barreto";
-$mensagem = "Seja bem-vindo(a) " . $nome . "!<br>";
-$date = date("d/m/Y");
+if($_SERVER['REQUEST_METHOD'] == 'GET'){
+    if(isset($_GET['user']) && isset($_GET['senha'])){
+
+        $user = $_GET['user'];
+        $senha = $_GET['senha'];
+
+        echo "Seu usuário é : " . "<br>";
+        echo $user . "<br>";
+        echo "Sua senha é: " . "<br>";
+        echo $senha;
+    }else{
+        echo "Informações não preenchidas!";
+    }
+}
 
 ?>
-
-<div class="conteudo">
-    <p>
-        <?php echo $mensagem ?>
-    </p>
-    <p>
-        <?php echo $date ?>
-    </p>
-</div>
 
 </body>
 </html>
