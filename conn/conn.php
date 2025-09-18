9f38bd7f4f12d45651d85639e064cc82fafa9972
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-09-18 16:42:37 -0300

    create table

--- conn/conn.php
+++ conn/conn.php
@@ -3,18 +3,26 @@
 $servername = "localhost";
 $username = "root";
 $password = "root";
+$dbname = "myDB";
 
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 if($conn->connect_error){
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "CREATE DATABASE myDB";
+$sql = "CREATE TABLE MyGuests(
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+
 if($conn->query($sql) === TRUE){
-    echo "Database created succesfully";
+    echo "Table MyGuests created succesfully";
 }else{
-    echo "Error creating database: " . $conn->error;
+    echo "Error creating table: " . $conn->error;
 }
 
 $conn->close();
