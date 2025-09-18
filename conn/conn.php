f79a0bf324a50b990d93f5129c7ce6ccacecaf51
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-09-18 16:35:57 -0300

    criar db com php sql

--- conn/conn.php
+++ conn/conn.php
@@ -9,6 +9,14 @@ $conn = new mysqli($servername, $username, $password);
 if($conn->connect_error){
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected succesfully";
+
+$sql = "CREATE DATABASE myDB";
+if($conn->query($sql) === TRUE){
+    echo "Database created succesfully";
+}else{
+    echo "Error creating database: " . $conn->error;
+}
+
+$conn->close();
 
 ?>
